ac400e8e7a442e03c6a0c52da2c97b08aea77f68
ilyaroadar <ilyaroadar@gmail.com>
2020-10-18 00:18:18 +0300

    add check of variable is exist

--- srm.sh
+++ srm.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+file_name="$1"
+
+if [ -z "$file_name" ]
+then
+	echo 'Укажите, пожалуйста, файл, который вы хотите удалить'
+	exit -1
+else
+	echo $file_name
+fi
+
 if [ -e ~/RECYCLE ]
 then
 	echo 'Директория RECYCLE существует'

755ad94efbf1a5dd512dfe0b290ae4765e186d2e
ilyaroadar <ilyaroadar@gmail.com>
2020-10-18 03:22:04 +0300

    add removing old files

--- srm.sh
+++ srm.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+seven_days_in_seconds=604800
+
+function get_current_timestamp {
+	date +"%s"
+}
+
+current_timestamp=$(get_current_timestamp)
+
+function get_newest_file_name {
+	ls ~/RECYCLE/ -tr | head -1
+}
+
+newest_file_name=$(get_newest_file_name)
+echo $newest_file_name
+
+function get_file_created_time {
+	stat -c %y ~/RECYCLE/${newest_file_name}
+}
+
+file_created_date=$(get_file_created_time)
+
+function convert_date_to_timestamp {
+	date -d $1 +"%s"
+}
+
+file_created_timestamp=$(convert_date_to_timestamp ${file_created_date})
+
+function get_difference {
+	expr $1 - $2
+}
+
+#todo: заменить условие на цикл
+if (( $(get_difference $current_timestamp $file_created_timestamp) >= $seven_days_in_seconds ))
+then	
+	echo 'Удаляем файл из корзины, так как он был перемещен в корзину более недели назад'
+	rm ~/RECYCLE/${newest_file_name}
+else
+	echo 'В корзине не осталось устаревших файлов'
+fi
+
 file_name="$1"
 
 if [ -z "$file_name" ]
